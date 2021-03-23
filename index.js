1f7b5e3c5b184f7c3766fb4aebf8e2c61e58645a
“[math95k2]” <[math95k2@stud.kea.dk]>
2021-03-23 22:18:44 +0100

    animationer

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from("#mission", {
-  scaleX: 0.5,
-  scaleY: 0.5,
-  scrollTrigger: {
-    trigger: "#mission",
-    start: "top 80%",
-    end: "top 50%",
-    scrub: true,
-    markers: true,
-  },
-});
+const sections = document.querySelectorAll("section");
+document.documentElement.style.setProperty("--animate-duration", "2s");
 
-$(function () {
-  $("a[href*=#]").on("click", function (e) {
-    e.preventDefault();
-    $("html, body").animate(
-      { scrollTop: $($(this).attr("href")).offset().top },
-      500,
-      "linear"
-    );
+sections.forEach((section) => {
+  gsap.set(section, { css: { className: "animate__animated op-0" } });
+
+  ScrollTrigger.create({
+    trigger: "#mission",
+    start: "top center",
+    end: "top 100px",
+    toggleClass: "animate__fadeInLeft",
+    once: true,
+  });
+
+  ScrollTrigger.create({
+    trigger: "#ingredienser",
+    start: "top center",
+    end: "top 100px",
+    toggleClass: "animate__fadeInLeft",
+    once: true,
+  });
+
+  ScrollTrigger.create({
+    trigger: "#join",
+    start: "top center",
+    end: "top 100px",
+    toggleClass: "animate__fadeInRight",
+    once: true,
   });
 });
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#produkter",
+      start: "center 460px",
+      end: "bottom top",
+      scrub: true,
+      pin: true,
+    },
+  })
+  .from(".p3", { y: innerHeight * 1.5 })
+  .from(".p1", { y: innerHeight * 1.5 });
+
+console.log(innerHeight);
